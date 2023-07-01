0eb5d0427b72a24ecec6222fb0a988ae4fd9b3b0
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-07-01 11:19:01 -0300

    Update autenticacao.py
    c

--- autenticacao.py
+++ autenticacao.py
@@ -1,10 +1,6 @@
-import investpy
-import yfinance as yf
-import plotly.graph_objects as go
 import streamlit as st
 
-def autenticar_usuario(username, password):
-    # Implemente sua própria lógica de autenticação aqui
+def autenticar_usuario(username, password):    
     # Verifique se o nome de usuário e a senha correspondem a um usuário válido
     # Retorne True se a autenticação for bem-sucedida, ou False caso contrário
 
@@ -18,61 +14,15 @@ def exibir_pagina_inicial():
     # Aqui você pode exibir a página inicial do seu aplicativo
     st.write("Página de autenticação!")
 
-def candlestick_with_line_chart(data):
-    fig = go.Figure()
-
-    # Gráfico de velas
-    fig.add_trace(go.Candlestick(x=data.index,
-                                 open=data['Open'],
-                                 high=data['High'],
-                                 low=data['Low'],
-                                 close=data['Close'],
-                                 name='Candles'))
-
-    # Gráfico de linhas
-    fig.add_trace(go.Scatter(x=data.index,
-                             y=data['Close'],
-                             mode='lines',
-                             name='Line Chart'))
-
-    fig.update_layout(
-        title='Gráfico de Velas com Linha',
-        yaxis_title='Preço',
-        xaxis_rangeslider_visible=False
-    )
-
-    return fig
-
 # Código da aplicação #=======================================================
 
 def exibir_pagina_restrita():
     # Aqui você pode exibir a página restrita acessível somente após a autenticação
 
-    tickers = investpy.get_stocks_list(country='brazil')
-    tickers = sorted(tickers)
-
     st.sidebar.header('MENU DE AÇÕES DA B3')
-
-    ticker = st.sidebar.selectbox('Selecione uma ação: ', tickers)
-
-    ticker = ticker + ".SA"
-
-    st.sidebar.write('Total de ações da B3: ', len(tickers))
     
-    st.header(ticker)
-
-    st.write('Você selecionou ', ticker)
-
-    períodos = ['5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'max'] 
-
-    periodo = st.sidebar.selectbox('Selecione o período: ', períodos)
-
-    dados = yf.download(ticker, period=periodo)
-
     st.title('Gráfico de Velas com Linha')
-    fig = candlestick_with_line_chart(dados)
-    st.plotly_chart(fig)
-
+    
 # Fim do código da aplicação #=====================================
 def main():
     # Inicializa o estado de autenticação como False (não autenticado)
